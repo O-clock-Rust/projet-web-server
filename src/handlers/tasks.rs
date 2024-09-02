bc1b262a17ba8ae64dac814f8773c8cce993f462
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2024-09-02 19:19:01 +0200

    add task routes

--- src/handlers/tasks.rs
+++ src/handlers/tasks.rs
@@ -2,7 +2,7 @@ use actix_web::{web, HttpResponse, Responder};
 
 use crate::common::AppState;
 
-pub async fn tasks_get(data: web::Data<AppState>) -> impl Responder {
+pub async fn get_tasks(data: web::Data<AppState>) -> impl Responder {
     // HttpResponse::Ok().body("Tasks")
     match &data.tasks {
         Some(tasks) => format!("The tasks:\n {:#?}", tasks),
@@ -10,6 +10,18 @@ pub async fn tasks_get(data: web::Data<AppState>) -> impl Responder {
     }
 }
 
+pub async fn get_task_detail(id: web::Path<u32>) -> impl Responder {
+    format!("Task #{}", id)
+}
+
 pub async fn add_task(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
+}
+
+pub async fn edit_task(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+pub async fn remove_task(id: web::Path<u32>) -> impl Responder {
+    format!("Task #{} was deleted", id)
 }
--- src/tasks_config.rs
+++ src/tasks_config.rs
@@ -1,13 +1,22 @@
 use actix_web::web;
 
-use crate::handlers::tasks;
+use crate::handlers::tasks::{add_task, edit_task, get_task_detail, get_tasks, remove_task};
 
 pub fn tasks_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/tasks").service(
-            web::resource("")
-                .route(web::get().to(tasks::tasks_get))
-                .route(web::post().to(tasks::add_task)),
-        ),
+        web::scope("/tasks")
+            .service(
+                web::resource("")
+                    .route(web::get().to(get_tasks))
+                    .route(web::post().to(add_task)),
+            )
+            .service(
+                web::scope("/{task_id}").service(
+                    web::resource("")
+                        .route(web::get().to(get_task_detail))
+                        .route(web::put().to(edit_task))
+                        .route(web::get().to(remove_task)),
+                ),
+            ),
     );
 }
