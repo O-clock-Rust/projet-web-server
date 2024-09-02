47cbac7fbb4ec0bb26fc2dce870e682931674b8c
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2024-09-02 16:48:07 +0200

    add first routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/tasks")]
+async fn tasks_get() -> impl Responder {
+    HttpResponse::Ok().body("Tasks")
+}
+
+#[post("/tasks")]
+async fn tasks_post(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/api")
+                .service(index)
+                .service(tasks_get)
+                .service(tasks_post),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
