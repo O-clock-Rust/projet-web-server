42ae48b5f125d8bfef33d05d2de093b6a49bee48
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2024-09-02 17:12:16 +0200

    init a state

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+#[derive(Debug)]
+struct Task {
+    id: u32,
+    title: String,
+    description: String,
+    status: bool,
+}
+
+struct AppState {
+    app_name: String,
+    tasks: Option<Vec<Task>>,
+}
+
 #[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index(data: web::Data<AppState>) -> impl Responder {
+    // HttpResponse::Ok().body("Hello world!")
+    let app_name = &data.app_name;
+    format!("Welcome to {}!", app_name)
 }
 
 #[get("/tasks")]
-async fn tasks_get() -> impl Responder {
-    HttpResponse::Ok().body("Tasks")
+async fn tasks_get(data: web::Data<AppState>) -> impl Responder {
+    // HttpResponse::Ok().body("Tasks")
+    match &data.tasks {
+        Some(tasks) => format!("The tasks:\n {:#?}", tasks),
+        None => "No tasks at this moment.".to_string(),
+    }
 }
 
 #[post("/tasks")]
@@ -18,12 +37,17 @@ async fn tasks_post(req_body: String) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(
-            web::scope("/api")
-                .service(index)
-                .service(tasks_get)
-                .service(tasks_post),
-        )
+        App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("ToDo List"),
+                tasks: None,
+            }))
+            .service(
+                web::scope("/api")
+                    .service(index)
+                    .service(tasks_get)
+                    .service(tasks_post),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
